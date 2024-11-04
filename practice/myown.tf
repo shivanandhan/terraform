37424ad6832f616df971f896a3a8ec37ac2c7f50
Shiva <shivanandhan33@gmail.com>
2024-11-04 22:07:40 +0530

    aws ec2 terraform file

--- practice/myown.tf
+++ practice/myown.tf
@@ -1,7 +1,8 @@
-resource "aws_instance" "Frontend" {
+resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0c6e17b6efba619a0"]
   tags = {
-    name = "Frontend"
+    Name = "frontend"
   }
 }
